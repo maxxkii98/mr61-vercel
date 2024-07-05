2b5ebc3a57ad737ed6078748c98baaef5ad714a1
maxxkii98 <keemackky@gmail.com>
2024-07-05 07:55:17 +0700

    edit time

--- public/script.js
+++ public/script.js
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
             teamBName: "สวิตเซอร์แลนด์",
             teamACode: "gc",
             teamBCode: "ch",
-            matchDate: new Date("2024-07-05T22:00:00+07:00")
+            matchDate: new Date("2024-07-06T22:00:00+07:00")
         },
         {
             teams: "เนเธอร์แลนด์ vs ตุรกี",
@@ -158,7 +158,7 @@ document.addEventListener('DOMContentLoaded', function() {
             teamBName: "ตุรกี",
             teamACode: "nl",
             teamBCode: "tr",
-            matchDate: new Date("2024-07-05T22:00:00+07:00")
+            matchDate: new Date("2024-07-06T22:00:00+07:00")
         }
     ];
 
